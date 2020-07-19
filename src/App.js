5f3c2b68305621026379e1f1be79167e8240e6c0
arsalan <arsalanc.v2@gmail.com>
2020-07-19 15:35:14 +0800

    Add production endpoint

--- src/App.js
+++ src/App.js
@@ -10,7 +10,10 @@ import Checkout from './views/Customer/Checkout/Checkout';
 import Orders from './views/Customer/Orders/Orders';
 import Navbar from "./components/Navbar";
 
-const Endpoint = 'http://127.0.0.1:5000';
+const Endpoint = process.env.NODE_ENV === 'development' ? 
+  'http://127.0.0.1:5000' : 'https://ec-asia-app.azurewebsites.net:5000/';
+
+console.log()
 
 function App() {
   const [websocket, setWebsocket] = useState(null);
