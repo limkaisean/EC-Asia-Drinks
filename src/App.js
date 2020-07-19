78c994c0dee229c1a87c3a07d2ba5a5d65e5eaee
Arsalan Cheema <arsalanc.v2@gmail.com>
2020-07-19 15:36:25 +0800

    Fix test errors (#14)
    * Fix test errors
    
    * Update App.test.js
    
    * Add production endpoint

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useRef } from 'react';
 import socketIOClient from 'socket.io-client';
-import { Route, Switch  } from 'react-router-dom'
+import { BrowserRouter, Route, Switch  } from 'react-router-dom'
 import { Provider } from 'react-redux'
 
 import store from './redux/store';
@@ -10,7 +10,10 @@ import Checkout from './views/Customer/Checkout/Checkout';
 import Orders from './views/Customer/Orders/Orders';
 import Navbar from "./components/Navbar";
 
-const Endpoint = 'http://127.0.0.1:5000';
+const Endpoint = process.env.NODE_ENV === 'development' ? 
+  'http://127.0.0.1:5000' : 'https://ec-asia-app.azurewebsites.net:5000/';
+
+console.log()
 
 function App() {
   const [websocket, setWebsocket] = useState(null);
@@ -31,6 +34,7 @@ function App() {
   return (
       <div className="App">
         <main>
+        <BrowserRouter>
           <Navbar />
           <Switch>
             <Provider store={store}>
@@ -40,6 +44,7 @@ function App() {
               <Route path="/orders" render={props => <Orders websocket={websocket} />} />
             </Provider>
           </Switch>
+        </BrowserRouter>
         </main>
       </div>
   );
