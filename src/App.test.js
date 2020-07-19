6af7ff4b7df50497e3025eb9cef2c916d881dbc8
Arsalan Cheema <arsalanc.v2@gmail.com>
2020-07-19 15:22:01 +0800

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders checkout link', () => {
+test('renders checkout page link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Checkout/i);
   expect(linkElement).toBeInTheDocument();
