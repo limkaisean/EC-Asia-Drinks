3501f553bbc2ccf8aa1d765af71e9baac1d44a6d
limkaisean <43609546+limkaisean@users.noreply.github.com>
2020-08-18 16:42:18 +0800

    Order page bug

--- src/views/Customer/Orders/Orders.js
+++ src/views/Customer/Orders/Orders.js
@@ -21,7 +21,7 @@ function Orders(props) {
         });
 
         props.websocket.on('update_status_relay', data => {
-            setOrders(orders);
+            setOrders({});
         });
         
         props.websocket.emit('confirmed_orders_request', { meetingRoom: meetingRoom });
